fdb7b0468fd70aeab5a0df7db9fbb22edfb2a011
Thomas Boulongne <thomasboulongne@hotmail.com>
2016-12-05 11:36:30 +0100

    Create footer component

---
+++ src/containers/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+	<footer>
+	</footer>
+</template>
+
+<script>
+	
+// import Emitter from '../../core/Emitter';
+
+export default {
+
+	data() {
+		return {
+		};
+	},
+
+	mounted() {
+	},
+
+	methods: {
+	}
+}
+
+</script>
+
+<style lang="sass">
+</style>
