021dc9aa0c9f67800f700f7ef6e6e81305e080aa
Thomas Boulongne <thomasboulongne@hotmail.com>
2016-12-06 22:19:09 +0100

    Loading screen ok 👌🏻

--- src/containers/components/HomeLoading.vue
+++ src/containers/components/HomeLoading.vue
@@ -1,7 +1,10 @@
 <template>
 	<div id="loading">
-		<div class="bar-wrapper">
-			<span ref="bar"></span>
+		<img id="home-logo" src="/images/logo_sm_white.png" alt="">
+		<span id="home-percentage">{{percentage.value}}%</span>
+		<div id="hp-warning">
+			<img src="/images/headphones.svg" alt="">
+			<span>Better with headphones</span>
 		</div>
 	</div>
 </template>
@@ -14,21 +17,82 @@ export default {
 
 	data() {
 		return {
-			state: 0
+			state: 0,
+			percentage: {tween: 0, value: 0}
 		};
 	},
 
 	mounted() {
 		Emitter.on('OBJ_LOADED', this.updateLoading);
+		let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		
+		TweenLite.set(svg, {
+			attr: {	
+				height: window.innerHeight,
+				width: window.innerWidth
+			}
+		});
+
+		let cx = window.innerWidth / 2;
+		let cy = window.innerHeight / 2;
+
+		let lastR = 1;
+		for (let i = lastR; i > 0; i -= 0.35) {
+			svg.innerHTML += '<circle cx="' + cx + '" cy="' + cy + '" r="' + window.innerHeight * i + '" stroke-width="1" stroke="#262626"/></circle>';
+			lastR = i;
+		}
+
+		this.$el.appendChild(svg);
+
+
+		let svgProgress = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		
+		TweenLite.set(svgProgress, {
+			filter: 'drop-shadow(0px 0px 7px #FFFFFF)',
+			position: 'absolute',
+			top: 0,
+			left:0,
+			attr: {
+				height: window.innerHeight,
+				width: window.innerWidth
+			}
+		});
+
+		let radius = window.innerHeight * lastR;
+		svgProgress.innerHTML += '<circle cx="' + cx + '" cy="' + cy + '" r="' + radius + '" stroke-width="4" stroke="#FFFFFF" transform="rotate(-90, ' + cx + ', ' + cy + ')"/></circle>';
+
+		this.progressCircle = svgProgress.childNodes[svgProgress.childNodes.length - 1];
+
+		this.progressCircle.pathLength = radius * Math.PI * 2;
+
+		TweenLite.set(this.progressCircle, {
+			attr: {
+				'stroke-dasharray': this.progressCircle.pathLength,
+				'stroke-dashoffset': -this.progressCircle.pathLength,
+				'stroke-width': 3
+			}
+		});
+
+		this.$el.appendChild(svgProgress);
 	},
 
 	methods: {
 		updateLoading(item, loaded, total) {
 			this.state = loaded / total;
-			TweenLite.to(this.$refs.bar, 1, { width: this.state * 100 + '%'});
+
+
+			TweenLite.to(this.percentage, 1, {
+				'tween': this.state * 100,
+				onUpdate: () => { this.percentage.value = parseInt(this.percentage.tween);}
+			});
+
+			TweenLite.to(this.progressCircle, 1, {
+				'stroke-dashoffset': -this.progressCircle.pathLength * (1 - this.state)
+			});
+
 			if (this.state == 1) {
 				setTimeout(()=>{Emitter.emit('LOADING_COMPLETE');}, 1000);
-				TweenLite.to(this.$el, 1, {opacity: 1 - this.state, delay: 2, onComplete: ()=>{
+				TweenLite.to(this.$el, 1, {opacity: 0, delay: 2, onComplete: ()=>{
 					TweenLite.set(this.$el, {display: 'none'});
 					Emitter.off('OBJ_LOADED', this.updateLoading);
 				}});
@@ -49,24 +113,43 @@ export default {
 		z-index: 2;
 		background-color: #000000;
 		opacity: 1;
-		
-		.bar-wrapper {
+
+		#home-logo {
 			position: absolute;
+			z-index: 2;
 			top: 50%;
 			left: 50%;
+			width: 20vw;
 			transform: translate(-50%, -50%);
-			display: inline-block;
-			border: 1px white solid;
-			width: 10vw;
-			height: 0.3vh;
-			background-color: transparent;
+		}
+
+		#home-percentage {
+			position: absolute;
+			z-index: 2;
+			top: 65%;
+			left: 50%;
+			font-size: 30px;
+			color: #cdcdcd;
+			transform: translate(-50%, -50%);
+		}
+
+		#hp-warning {
+			text-align: center;
+			position: absolute;
+			z-index: 2;
+			left: 50%;
+			bottom: 5%;
+			transform: translateX(-50%);
+			text-transform: uppercase;
 			span {
-				position: absolute;
-				left: 0;
-				top: 0;
-				height: 100%;
-				width: 0;
-				background-color: white;
+				margin-top: 1em;
+				display: block;
+				font-size: 14px;
+				font-family: 'Gotham-Book'
+			}
+
+			img {
+				margin: auto;
 			}
 		}
 	}
